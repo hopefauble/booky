8d0a709171c7a5cb3bf61a53c7feb951281761b2
Hope Fauble <97677671+hopha88@users.noreply.github.com>
2024-04-30 12:55:16 -0400

    Book & Completed Database Entries Implemented with full CRUD and functional. (#14)
    * API Call to Google Library now pushes book complete information to the backend database.
    
    Now the booklist feature can directly fetch information from the database, instead of needing to rely on another API call. WE previously only stored information about ISBN, however not all books have this information. Books ae now stored with an id assigned by the database to uniquely identity each Book object.
    
    Notable changes:
    - Book model in frontend has all values set to datatype "string".
    - API call passes in book data through the request body instead of parameters (and interpreted through the json bodyparser in app.mjs) to enure the preservation of the data.
    
    * Resetting database information, preventing adding of test data.
    
    * Adding some test data for usage in booklist.
    
    * Implementation for Completed table complete, bug fixes in Book.
    
    You can not make two different deletes from the Completed table, one that completed gets rid of the book, the other that returns the book back to the reading list.
    
    The reading list will only have one delete. To "move to finished reading", simply run a post call to /completed/id where the id is the one of the book from the booklist, and it will run the deletion on its own.
    
    Attempted to begin implementing the update, but not working yet.
    
    * Update method/ call implemented and functional.
    
    * Update Book.mjs
    
    * Update Book.mjs

--- Book.mjs
+++ Book.mjs
@@ -1,4 +1,4 @@
-import {db} from './db.mjs';
+import { db } from './db.mjs';
 export class Book {
 
     #id
@@ -6,28 +6,28 @@ export class Book {
     #title
     #authors
     #description
+    #notes
 
-    constructor (id, isbn, title, authors, description) {
+    constructor(id, isbn, title, authors, description, notes) {
         this.#id = id;
         this.#isbn = isbn;
         this.#title = title;
         this.#authors = authors;
-        this.#description =  description;
+        this.#description = description;
+        this.#notes = notes;
     }
 
     static async create(data) {
-        // if ((data !== undefined) && (data instanceof Object) 
-        // ) {
-            try {
-                let db_result = await db.run('insert into Books values (NULL, ?, ?, ?, ?)', data.isbn, data.title, data.authors, data.description);
-                let ing = new Book(db_result.lastID, data.isbn, data.title, data.authors, data.description);
-                return ing;
-            } catch (e) {
-                return null;
-            }
+        try {
+            // Stretch two: check if the book already exists. If so, return "Book exists."
+            let db_result = await db.run('insert into Books values (NULL, ?, ?, ?, ?, ?)', data.isbn, data.title, data.authors, data.description, "");
+            let ing = new Book(db_result.lastID, data.isbn, data.title, data.authors, data.description, "");
+            return ing;
+        } catch (e) {
+            return null;
         }
-        // return null;
-    // }
+    }
+
 
     static async getAllIDs() {
         try {
@@ -53,7 +53,7 @@ export class Book {
             if (!row) {
                 return null;
             } else {
-                return new Book(row.id, row.isbn, row.title, row.authors, row.description);
+                return new Book(row.id, row.isbn, row.title, row.authors, row.description, row.notes);
             }
         } catch (e) {
             console.error("Error fetching book by ID:", e);
@@ -65,12 +65,24 @@ export class Book {
 
     static async deleteBookByID(id) {
         try {
-            await db.run('delete from Books where id = ?', id);
+            await db.run('delete from books where id = ?', id);
             return true;
         } catch (e) {
             return false;
         }
     }
+
+    async setNotes(id, new_note) {
+        try {
+            await db.run('update books set notes = ? where id = ?', [new_note, id]);
+            this.#notes = new_note;
+            return true;
+        } catch (e) {
+            console.error("Error setting notes:", e);
+            return false;
+        }
+    }
+
 
     json() {
         return {
@@ -78,7 +90,8 @@ export class Book {
             isbn: this.#isbn,
             title: this.#title,
             authors: this.#authors,
-            description: this.#description
+            description: this.#description,
+            notes: this.#notes
         }
     }
 
@@ -86,13 +99,24 @@ export class Book {
         return this.#id;
     }
 
-    async setisbn(new_isbn) {
-        try {
-            await db.run('update Books set isbn = ? where id = ?', this.#isbn, this.#id);
-            this.#isbn = new_isbn;
-            return true;
-        } catch (e) {
-            return false;
-        }
+    getISBN() {
+        return this.#isbn;
     }
+
+    getTitle() {
+        return this.#title;
+    }
+
+    getAuthors() {
+        return this.#authors
+    }
+
+    getDescription() {
+        return this.#description
+    }
+
+    getNotes() {
+        return this.#notes
+    }
+
 }
